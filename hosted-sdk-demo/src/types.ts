c1893f808f583aacc2916df6aaaab1a83a977bab
yahuy-pendle <gia.huy@pendle.finance>
2024-12-19 16:48:13 +0700

    Add cancel limit order

--- hosted-sdk-demo/src/types.ts
+++ hosted-sdk-demo/src/types.ts
@@ -9,3 +9,36 @@ export type RedeemPyData = { amountOut: string, priceImpact: number };
 export type RedeemSyData = { amountOut: string, priceImpact: number };
 export type TransferLiquidityData = { amountLpOut: string; amountYtOut: string, priceImpact: number };
 export type RollOverPtData = { amountPtOut: string, priceImpact: number };
+
+export interface LimitOrderResponse {
+    /** Hash of the order */
+    id: string;
+    /** Signature of order, signed by maker */
+    signature: string;
+    /** Chain id */
+    chainId: number;
+    /** BigInt string of salt */
+    salt: string;
+    /** BigInt string of expiry, in second */
+    expiry: string;
+    /** BigInt string of nonce */
+    nonce: string;
+    /** LimitOrderType { 0 : TOKEN_FOR_PT, 1 : PT_FOR_TOKEN, 2 : TOKEN_FOR_YT, 3 : YT_FOR_TOKEN } */
+    type: 0 | 1 | 2 | 3;
+    /** Token used by user to make order */
+    token: string;
+    /** YT address */
+    yt: string;
+    /** Maker address */
+    maker: string;
+    /** Receiver address */
+    receiver: string;
+    /** BigInt string of making amount, the amount of token if the order is TOKEN_FOR_PT or TOKEN_FOR_YT, otherwise the amount of PT or YT */
+    makingAmount: string;
+    /** BigInt string of remaining making amount, the unit is the same as makingAmount */
+    lnImpliedRate: string;
+    /** BigInt string of failSafeRate */
+    failSafeRate: string;
+    /** Bytes string for permit */
+    permit: string;
+}
