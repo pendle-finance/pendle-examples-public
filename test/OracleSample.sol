1906624848ac2ed4b9d874c5130d6239c42725a0
Tran Quang Loc <quangloctran99@gmail.com>
2025-05-14 11:59:01 +0700

    Add example that uses Oracle Library

--- test/OracleSample.sol
+++ test/OracleSample.sol
@@ -35,6 +35,10 @@ contract OracleSample is PRBTest, StdCheats, StructGen {
     _test_oracle_ready(address(market), twapDuration);
   }
 
+  ////////////////
+  //// Using Oracle
+  ////////////////
+
   function test_get_price_LRT_in_underlying() external view {
     uint256 ptRateInWeEth = oracle.getPtToSyRate(address(market), twapDuration);
     uint256 ytRateInWeEth = oracle.getYtToSyRate(address(market), twapDuration);
@@ -65,9 +69,28 @@ contract OracleSample is PRBTest, StdCheats, StructGen {
     console.log("1 PT = %s USD (base 1e18)", ptRateInUsd);
   }
 
-  // ! Replace oracle.getPtToAssetRate to IPMarket(market).getPtToAssetRate to save 3000 gas
-  function test_get_price_save_gas() external view {
+  ////////////////
+  //// Using Lib
+  ////////////////
+
+  function test_get_price_LRT_in_underlying_with_lib() external view {
     uint256 ptRateInWeEth = market.getPtToSyRate(twapDuration);
+    uint256 ytRateInWeEth = market.getYtToSyRate(twapDuration);
+    uint256 lpRateInWeEth = market.getLpToSyRate(twapDuration);
+
+    console.log("1 PT = %s Wrapped eEth (base 1e18)", ptRateInWeEth);
+    console.log("1 YT = %s Wrapped eEth (base 1e18)", ytRateInWeEth);
+    console.log("1 LP = %s Wrapped eEth (base 1e18)", lpRateInWeEth);
+  }
+
+  function test_get_price_LRT_with_external_oracle_with_lib() external view {
+    uint256 ptRateInWeEth = market.getPtToSyRate(twapDuration); // 1 SY-weETH = 1 weETH
+
+    uint256 ptRateInEth = (ptRateInWeEth * uint256(weETH_ETH_feed.latestAnswer())) / (10 ** weETH_ETH_feed.decimals());
+    console.log("1 PT = %s ETH (base 1e18)", ptRateInEth); // 1 PT = 0.980103943942239852 ETH
+
+    uint256 ptRateInUsd = (ptRateInEth * uint256(ETH_USD_feed.latestAnswer())) / (10 ** ETH_USD_feed.decimals());
+    console.log("1 PT = %s USD (base 1e18)", ptRateInUsd); // 1 PT = 3714.1302603652102 USD
   }
 
   /// @dev Call only once for each (market, duration). Once successful, it's permanently valid (also for any shorter
